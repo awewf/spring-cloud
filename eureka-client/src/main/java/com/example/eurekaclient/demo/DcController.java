23a184dd7b53451912c6f521e3f61b6c8b2745c7
linsen <1670856343@qq.com>
2019-08-29 11:40:52 +0800

    springcloud

---
+++ eureka-client/src/main/java/com/example/eurekaclient/demo/DcController.java
@@ -0,0 +1,27 @@
+package com.example.eurekaclient.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName DcController
+ * @Description TODO
+ * @Author GuanLS
+ * @Date 2019/8/28 17:31
+ * @Version 1.0
+ **/
+
+@RestController
+public class DcController {
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc(){
+        String service = "Service:"+discoveryClient.getServices();
+        System.out.println(service);
+        return service;
+    }
+}
